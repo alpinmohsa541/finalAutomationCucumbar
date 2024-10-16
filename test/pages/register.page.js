7026dd10ace74216a0fb5b84a1c22e0053606ef7
alpinmohsa541 <lpinmohsa@gmail.com>
2024-10-16 14:58:50 +0700

    add scenario register

---
+++ test/pages/register.page.js
@@ -0,0 +1,51 @@
+class RegisterPage {
+    //TODO 1: define 'web element locator' here
+    get RegisterPageUrl() {
+      return '/register';
+    }
+  
+    get NameInput() {
+        return $('//*[@id="name"]'); //locator field: email
+      }
+
+    get emailInput() {
+      return $('#email'); //locator field: email
+    }
+  
+    get passwordInput() {
+      return $('#password'); //locator field: password
+    }
+  
+    get RegisterButton() {
+      return $(
+        '/html/body/div[1]/div/div/div/div[2]/div/button'
+      ); //locator field: Register Button
+    }
+  
+    get errorMsg() {
+      return $('//*[@role="alert"]');
+    }
+  
+    //TODO 2: define 'element actions' here
+    async register(name,email, password) {
+      await this.NameInput.setValue(name);
+      await this.emailInput.setValue(email);
+      await this.passwordInput.setValue(password);
+      await this.RegisterButton.click();
+    }
+  
+    async assertRegisterPageUrl() {
+      await expect(browser).toHaveUrl(this.RegisterPageUrl);
+    }
+  
+    async assertErrorMessage(expectedErrorMessage) {
+      const actualErrorMessage = await this.errorMsg.getText(); // Ambil teks dari elemen
+      expect(actualErrorMessage).toContain(expectedErrorMessage); // Gunakan .toContain untuk memeriksa isi teks
+  }
+  
+    async getErrorMessageText() {
+      return await this.errorMsg.getText();
+    }
+  }
+  
+  module.exports = new RegisterPage();
